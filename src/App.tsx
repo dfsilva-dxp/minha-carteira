13f8375d39015668acc913fdd92144d6880d1744
Daniel Silva <dani3l.dxp@gmail.com>
2021-08-26 14:21:46 -0300

    feat: importando o tema

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
+import { ThemeProvider } from "styled-components";
+
 import Layout from "./components/Layout";
+
 import { GlobalStyles } from "./styles/global";
+import { dark } from "./styles/themes/dark";
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={dark}>
       <GlobalStyles />
       <Layout>
         <p>Content</p>
       </Layout>
-    </>
+    </ThemeProvider>
   );
 }
 

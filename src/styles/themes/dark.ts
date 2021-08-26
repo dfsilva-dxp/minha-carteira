eb99647fce389a963a27f92f6eff2ff63a97ad03
Daniel Silva <dani3l.dxp@gmail.com>
2021-08-26 14:21:04 -0300

    feat: definindo as cores dos temas dark e light

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,16 @@
+export const dark = {
+  title: "dark",
+  waves: "waves-dark-theme.svg",
+  color: {
+    background: "#191a1c",
+    text: "#fff",
+    shape: "#28292D",
+
+    white: "#fff",
+    gray400: "#A8A8B3",
+    blue700: "#3585f3",
+    green500: "#52d987",
+    yellow500: "#eba417",
+    red500: "#e83f5b",
+  },
+};

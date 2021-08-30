ff2abb70219d5f8f30176da42a4c0132a2b7b916
Daniel Silva <dani3l.dxp@gmail.com>
2021-08-30 17:53:05 -0300

    feat: implementando o component filter

--- src/components/Filter/index.tsx
+++ src/components/Filter/index.tsx
@@ -1,19 +1,45 @@
+import { SyntheticEvent, useRef, useState } from "react";
 import { RiCalendar2Line, RiArrowUpSLine } from "react-icons/ri";
+
 import InputFilterMonthAndYear from "../Inputs/InputFilterMonthAndYear";
+
+import { useOutsideClick } from "../../hooks/useOutsideClick";
 
 import * as S from "./styles";
 
 const Filter = () => {
+  const [value, setValue] = useState("todos");
+
+  const dropDownRef = useRef<HTMLDivElement>(null);
+  const [isActive, setIsActive] = useOutsideClick(dropDownRef, false);
+
+  function handleUseOutsideClick() {
+    setIsActive(!isActive);
+  }
+
+  function handleChange(event: SyntheticEvent) {
+    const value = (event.target as HTMLInputElement).value;
+    const date = new Date(value).toLocaleDateString("pt-BR", {
+      month: "long",
+      year: "numeric",
+    });
+    setValue(String(date));
+    setIsActive(!isActive);
+  }
+
   return (
     <S.Wrap>
-      <S.FilterButton>
+      <S.FilterButton onClick={handleUseOutsideClick}>
         <RiCalendar2Line />
-        <strong>Todos</strong>
-        <RiArrowUpSLine />
+        <strong>{value}</strong>
+        <RiArrowUpSLine className={isActive ? "rotate-in" : "rotate-out"} />
       </S.FilterButton>
-      <S.DropdownButton>
-        <InputFilterMonthAndYear />
-      </S.DropdownButton>
+      <S.DropdownContent
+        className={isActive ? "fade-in" : ""}
+        ref={dropDownRef}
+      >
+        <InputFilterMonthAndYear handleChange={handleChange} value={value} />
+      </S.DropdownContent>
     </S.Wrap>
   );
 };

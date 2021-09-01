2d9599fbd7cc573600a39f858d0dcc55f6cfe38e
Daniel Silva <dani3l.dxp@gmail.com>
2021-09-01 15:42:32 -0300

    feat: recebendo as transactions, filtrando por tipo e passando para a lista

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,4 +1,7 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router";
+
+import { useTransaction } from "../../hooks/useTransaction";
 
 import PageContentTitle from "../../components/PageContentTitle";
 import RadioButtonTransactionType from "../../components/RadioButtonTransactionType";
@@ -6,14 +9,38 @@ import TransactionTable from "../../components/TransactionTable";
 
 import * as S from "./styles";
 
+type Transaction = {
+  id: number;
+  title: string;
+  amount: number;
+  type: string;
+  category: string;
+  frequency: string;
+  create_at: string;
+};
+
 const Transactions = () => {
+  const [transactionsFilter, setTransactionsFilter] = useState<Transaction[]>(
+    []
+  );
   const { type } = useParams();
+  const { transactions } = useTransaction();
+
+  const transactionType = type === "saidas" ? "withdraw" : "deposit";
+
+  useEffect(() => {
+    const response = transactions.filter(
+      (item) => item.type === transactionType
+    );
+
+    setTransactionsFilter(response);
+  }, [transactionType, transactions]);
 
   return (
     <S.Container>
       <PageContentTitle title={type} />
       <RadioButtonTransactionType />
-      <TransactionTable />
+      <TransactionTable transactions={transactionsFilter} />
     </S.Container>
   );
 };

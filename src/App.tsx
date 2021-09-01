5e05536e47a5de68e5e3c49ec4576c74e21323c6
Daniel Silva <dani3l.dxp@gmail.com>
2021-09-01 12:02:54 -0300

    feat: recebendo as rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
+import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 
 import Layout from "./components/Layout";
-import Transactions from "./pages/Transactions";
+import AppRoutes from "./routes";
 
 import { GlobalStyles } from "./styles/global";
 import { light } from "./styles/themes/light";
 
 function App() {
   return (
-    <ThemeProvider theme={light}>
-      <GlobalStyles />
-      <Layout>
-        <Transactions />
-      </Layout>
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={light}>
+        <GlobalStyles />
+        <Layout>
+          <AppRoutes />
+        </Layout>
+      </ThemeProvider>
+    </BrowserRouter>
   );
 }
 
